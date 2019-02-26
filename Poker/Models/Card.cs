82cfdced92838be22e2fe7f148eb21d9b93a450f
shital <ssbhutiya@gmail.com>
2019-02-26 02:07:03 -0600

    Removed unnecessary part

--- Poker/Models/Card.cs
+++ Poker/Models/Card.cs
@@ -1,32 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-public enum Suit
-{
-    Clubs,
-    Diamonds,
-    Hearts,
-    Spades
-}
-public enum Rank
-{
-    Ace,
-    Two,
-    Three,
-    Four,
-    Five,
-    Six,
-    Seven,
-    Eight,
-    Nine,
-    Ten,
-    Jack,
-    Queen,
-    King
-}
-
+﻿
 namespace Poker.Models
 {
     public class Card
